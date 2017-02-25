11fa6fd65dec0e6087019bd0861c1a5d5518993f
danmenglover <danmenglove3@163.com>
2017-02-25 11:22:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/mydow/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.mydow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for(int i=0;i<10;i++){
+            Log.e("log","你好");
+        }
+    }
+}
